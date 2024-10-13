7ce60371db594040c859075913508f0f9035ddaa
Kamrul Hassan <kamrulthedev@gmail.com>
2024-10-13 17:33:48 +0600

    create-verify-commit

--- src/app/modules/Verify/verify.service.ts
+++ src/app/modules/Verify/verify.service.ts
@@ -4,30 +4,40 @@ import { User } from '../User/user.model';
 import { Verify } from './verify.model';
 
 const verifyAccount = async (verifyData: any) => {
-  const { userId, name, email, phone, addess } = verifyData;
-
+  const { userId, ammount, name, email, phone, addess } = verifyData;
   const user = await User.findById(userId);
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'User Is Found');
   }
-
-  if(user.status === "BLOCKED"){
-   throw new AppError(httpStatus.BAD_REQUEST, "User Is BlockedF")
+  if (user.status === 'BLOCKED') {
+    throw new AppError(httpStatus.BAD_REQUEST, 'User Is BlockedF');
   }
-
-  const booking = new Verify({
+  const AccountData = new Verify({
     user: userId,
     name,
+    ammount,
     email,
     phone,
     addess,
   });
-  const verify = await booking.save();
+  const verify = await AccountData.save();
   await verify.populate('user');
-
   return verify;
+};
+
+//get all Verify Account
+const getAllVerifyAccount = async () => {
+  const result = await Verify.find().populate('user');
+  return result;
+};
+
+const getMyVerifayAccoount = async (userId: any) => {
+  const result = await Verify.find(userId).populate('user');
+  return result;
 };
 
 export const VerifyService = {
   verifyAccount,
+  getAllVerifyAccount,
+  getMyVerifayAccoount,
 };
